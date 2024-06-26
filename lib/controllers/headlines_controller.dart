cb1a58c012483ac59119fc7eb55c7de494599426
Ashok <91631998+AshokRenangi1903@users.noreply.github.com>
2024-06-26 18:26:36 +0530

    Update headlines_controller.dart
    api key changed

--- lib/controllers/headlines_controller.dart
+++ lib/controllers/headlines_controller.dart
@@ -6,7 +6,7 @@ import 'package:news_app/models/headlines_model.dart';
 class HeadlinesController {
   Future getHeadlines(String newsChannel) async {
     var headlinesUrl =
-        "https://newsapi.org/v2/top-headlines?sources=${newsChannel}&apiKey=5b39f8103d8640e4839bb2be4b4b9661";
+        "https://newsapi.org/v2/top-headlines?sources=${newsChannel}&apiKey=<YOUR-API-KEY>";
 
     var response = await http.get(Uri.parse(headlinesUrl));
 
